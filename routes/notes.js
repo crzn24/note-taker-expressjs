a8c4ca010cf78be4de3fd9ea6e2a7b7d132175d1
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-12-10 13:44:31 -0800

    Adds notes to notes.js and starts work on post route for new note

--- routes/notes.js
+++ routes/notes.js
@@ -1,21 +1,21 @@
-// router
+// Router for notes
 
 const notes = require('express').Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
 
-// GET Route for retrieving all the tips
+// GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
   console.info(`${req.method} request received for notes`);
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-// POST Route for a new UX/UI tip
+// POST Route for a new note
 notes.post('/', (req, res) => {
-  console.info(`${req.method} request received to add a tip`);
+  console.info(`${req.method} request received to add a note`);
   console.log(req.body);
 
-//   const { username, topic, tip } = req.body;
+  const { title, text } = req.body;
 
 //   if (req.body) {
 //     const newTip = {
