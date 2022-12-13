d186cc041b145bc69ad5ed853e22b9ca86ce70bb
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2022-12-12 18:19:40 -0800

    Edits comments and deletes commented out code

--- routes/notes.js
+++ routes/notes.js
@@ -36,14 +36,13 @@ notes.post('/', (req, res) => {
 // BONUS Delete request
 // DELETE /api/notes/:id should receive a query parameter that contains the id of a note to delete. To delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
 notes.delete('/:id', (req, res) => {
-  // console.log('--- delete route hit ---')
   console.info(`${req.method} request received to delete a note`);
 
   
   readFromFile('./db/db.json').then((data) => {
     let notes = JSON.parse(data);
-    // console.log(req.params.id);
-    // console.log(notes);
+    // use console.log(req.params.id); to view the id number of the note you want to delete
+    // use console.log(notes); to see the notes array
     // use filter method on the notes array to filter out the deleted note by checking on the id value, and then write to the file with the newly filtered array.
     
     const filteredNotes = notes.filter(note => note.id !== req.params.id);
